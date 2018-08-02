4a4dd9540ada647985d765ac89fd6035499c95b9
Nuno <nunofpfaria@gmail.com>
2018-08-02 14:31:50 +0100

    Added player and ball prefabs, table borders and kick animation
    Added gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("kick", false);
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision) {
+        Debug.Log("asdasd");
+        if (collision.name == "ball") {
+            anim.SetBool("kick", true);
+            //TODO change
+            collision.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.name == "ball") {
+            anim.SetBool("kick", false);
+        }
+    }
+}

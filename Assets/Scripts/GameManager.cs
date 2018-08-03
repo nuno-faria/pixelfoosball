26e140dc95a861d92f422eb6fc8f5f2c00f5fe78
Nuno <nunofpfaria@gmail.com>
2018-08-03 14:55:09 +0100

    Added both team players, movements, ball reaction to player and spawning
    Updated board

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+
+    public GameObject ball;
+    public Rigidbody2D ballRB;
+
+	void Start () {
+        gm = this;
+        ballRB = ball.GetComponent<Rigidbody2D>();
+        NewRound();
+	}
+
+    public void NewRound() {
+        ball.transform.position = new Vector2(0f, 0f);
+        ballRB.velocity = new Vector2(0f, 0f);
+        Invoke("GoBall", 0.8f);
+    }
+
+    public void GoBall() {
+        float x = Random.Range(0f, 1f);
+        if (x <= 0.5)
+            ballRB.AddForce(new Vector2(12f, 6f));
+        else ballRB.AddForce(new Vector2(-12f, 6f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public Animator anim;
-
+    public string direction;
+    private Rigidbody2D playerRB;
+    public KeyCode up;
+    public KeyCode down;
+    public float speed = 6;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         anim.SetBool("kick", false);
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update() {
+        if (Input.GetKey(up))
+            playerRB.velocity = new Vector2(0, speed);
+
+        else if (Input.GetKey(down))
+            playerRB.velocity = new Vector2(0, -speed);
+
+        else playerRB.velocity = new Vector2(0, 0);
     }
 
     private void OnTriggerEnter2D (Collider2D collision) {
-        Debug.Log("asdasd");
         if (collision.name == "ball") {
             anim.SetBool("kick", true);
-            //TODO change
-            collision.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+            Rigidbody2D ballRB = collision.GetComponent<Rigidbody2D>();
+            float x = (2 + ballRB.velocity.x / 2) + (playerRB.velocity.y / 3f);
+            float y = (2 + ballRB.velocity.y / 2) + (playerRB.velocity.y / 2f);
+            if (direction == "right")
+                ballRB.velocity = new Vector2(Math.Min(Math.Abs(x), 10),y);
+            else ballRB.velocity = new Vector2(Math.Min(-1 * Math.Abs(x), -10), y);
         }
     }
 
